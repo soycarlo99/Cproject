171229bdb5fb2b541238eab91abcdcc2436b89ff
carlodelos90 <carlodelos90@gmail.com>
2024-12-13 23:59:14 +0100

    first C project

---
+++ first.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main() {
+  int myfacoritenum = 0;
+  
+  printf("Please enter your favoritenumber\n");
+  scanf("%d", &myfacoritenum);
+  printf("You Entered: %d\n", myfacoritenum);
+
+
+  return 0;}
