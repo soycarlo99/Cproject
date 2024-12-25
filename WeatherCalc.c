d38c14b677d0a4b39c7d39d7b9da912f8c5bde74
carlodelos90 <carlodelos90@gmail.com>
2024-12-25 23:09:36 +0100

    Weather Calculation mini project completed

--- WeatherCalc.c
+++ WeatherCalc.c
@@ -1,23 +1,27 @@
 #include <stdio.h>
 
+int main() {
+    float yearlyRain[5][12] = {
+        {3, 2, 5, 1, 5, 1, 2, 5, 5, 1, 1, 5},
+        {3, 2, 5, 1, 5, 2, 2, 5, 5, 1, 1, 5},
+        {3, 1, 3, 6, 2, 1, 2, 5, 5, 1, 1, 5},
+        {3, 2, 5, 1, 5, 1, 2, 5, 5, 1, 1, 5},
+        {3, 2, 5, 1, 5, 1, 2, 5, 5, 1, 1, 5}
+    };
 
-int main(){
-float yearlyRain[5][12] = {
-  {3,2,5,1,5,1,2,5,5,1,1,5},
-  {3,2,5,1,5,2,2,5,5,1,1,5},
-  {3,1,3,6,2,1,2,5,5,1,1,5},
-  {3,2,5,1,5,1,2,5,5,1,1,5},
-  {3,2,5,1,5,1,2,5,5,1,1,5}
-  };
+    for (int i = 0; i < 5; i++) {
+        int year = 2015;
+        float total = 0;
+        printf("\nYear %d: ", year + i);
 
-  for(int i = 0; i < 5; i++){
+        for (int j = 0; j < 12; j++) {
+            printf("%.2f, ", yearlyRain[i][j]);
+            total += yearlyRain[i][j];
+        }
 
-    int year = 2015;
-    printf("\n");
-    printf("Year %d: ", year+i);
-      for(int j = 0; j < 12; j++){
-      printf("%.2f, ", yearlyRain[i][j]);
+        float average = total / 12; 
+        printf("Average: %.2f", average); 
     }
-  }
-  return 0;
+
+    return 0;
 }
