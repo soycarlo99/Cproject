891a93dc885f4f4fdcf60689140b4ba958e88868
carlodelos90 <carlodelos90@gmail.com>
2024-12-25 14:55:52 +0100

    Updated the number guessing game program

--- guessNum.c
+++ guessNum.c
@@ -2,29 +2,36 @@
 #include <stdlib.h>
 #include <time.h>
 
-int main(){
-  time_t t;
-  srand((unsigned)time(&t));
-  int randomNumber = rand() % 21;
-  int guessed = 0;
+int main() {
+    time_t t;
+    srand((unsigned)time(&t));
+    int randomNumber = rand() % 21;
+    int guessed;
+    int lives = 5;
 
+    printf("This is a guessing game!\n");
+    printf("You need to choose a number between 0-20\n\n");
 
-  printf("This is a guessing game!\n");
-  printf("You need to choose a number between 0-20\n");
-  printf("\n\n");
-  printf("Please enter a number between 0-20\n");
-  scanf("%d", &guessed);
-  
-while(!(guessed==randomNumber)){
-      if(guessed<randomNumber){
-      printf("Please enter a HIGHER number\n");
-      } else if(guessed>randomNumber){
-      printf("please enter a LOWER number\n");
-      }
-      
-      scanf("%d", &guessed);
+    while (lives > 0) {
+        printf("You have %d live(s) left.\n", lives);
+        printf("Please enter a number between 0-20: ");
+        scanf("%d", &guessed);
+
+        if (guessed == randomNumber) {
+            printf("You guessed right!!!\n");
+            return 0; // Exit the game after a correct guess
+        } else if (guessed < randomNumber) {
+            printf("Please enter a HIGHER number.\n");
+        } else if (guessed > randomNumber) {
+            printf("Please enter a LOWER number.\n");
+        }
+
+        // Decrease lives after an incorrect guess
+        lives--;
     }
-      if(guessed==randomNumber){
-      printf("You guessed right!!!\n");
-  }
+
+    // If the loop exits, it means the player ran out of lives
+    printf("Sorry, you've run out of lives. The correct number was %d.\n", randomNumber);
+
+    return 0;
 }
