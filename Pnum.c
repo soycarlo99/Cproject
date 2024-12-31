aa5590d4b466151d8502fee4b56b3f5faa48f66a
carlodelos90 <carlodelos90@gmail.com>
2024-12-31 15:07:14 +0100

    Added another pointers project
    In this project I print out the adress of myNumber, adress of the Pnumber and Also the value of them both.

---
+++ Pnum.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stddef.h>
+#include <stdlib.h>
+int main(){
+  system("clear");
+  int myNumber = 10;
+  int *PmyNumber = NULL;
+  PmyNumber = &myNumber;
+
+
+  printf("1- The adress of myNumber is: %p\n", &myNumber);
+  printf("2- The adress of PmyNumber is: %p\n", &PmyNumber);
+  printf("3- The value of the PmyNumber in the memory(pointer - same as first line) is: %p\n", PmyNumber);
+  printf("4- The value of what PmyNumber is pointing to: %d\n", *PmyNumber);
+  return 0;
+}
