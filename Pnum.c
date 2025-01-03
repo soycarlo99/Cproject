379ed9eed4873ba5985f09501671a6ea73cae093
carlodelos90 <carlodelos90@gmail.com>
2025-01-03 21:14:07 +0100

    modified some comments for clear code

--- Pnum.c
+++ Pnum.c
@@ -7,7 +7,7 @@ int main(){
   int *PmyNumber = NULL;
   PmyNumber = &myNumber;
 
-
+  //%p for memory adress
   printf("1- The adress of myNumber is: %p\n", &myNumber);
   printf("2- The adress of PmyNumber is: %p\n", &PmyNumber);
   printf("3- The value of the PmyNumber in the memory(pointer - same as first line) is: %p\n", PmyNumber);
